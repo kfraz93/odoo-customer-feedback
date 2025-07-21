8f164818aeec90c98fcbfc93f1d6602a94f9ab8d
f.ahmad <kfraz93@live.com>
2025-07-21 16:02:21 +0200

    fix(car): Adding missing description field in car model

--- addons/cars/models/car.py
+++ addons/cars/models/car.py
@@ -6,8 +6,10 @@ class Car(models.Model):
     _name = "cars.car"
     _description = "Cars for sale"
     _order = "name_asc"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string="Car name", required=True, help="Make and model of the car")
+    description = fields.Text(string="Description", help="Detailed description of the car")
     model_year = fields.Integer(string="Model year", required=True, help="Year car was made")
     vin = fields.Char(string="VIN", copy=False, help="Vehicle registration number")
     cost_price = fields.Float(string="Cost Price", required=True, digits=(10,2), help="Purchase price of vehicle")
