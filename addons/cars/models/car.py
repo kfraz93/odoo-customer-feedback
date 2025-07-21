8580911281f22fad39a824d50e51b11b912440cd
f.ahmad <kfraz93@live.com>
2025-07-21 12:35:21 +0200

    feat(cars): Define Car model with fields, computed profit, and state

---
+++ addons/cars/models/car.py
@@ -0,0 +1,32 @@
+from email.policy import default
+
+from odoo import models, fields, api
+
+class Car(models.Model):
+    _name = "cars.car"
+    _description = "Cars for sale"
+    _order = "name_asc"
+
+    name = fields.Char(string="Car name", required=True, help="Make and model of the car")
+    model_year = fields.Integer(string="Model year", required=True, help="Year car was made")
+    vin = fields.Char(string="VIN", copy=False, help="Vehicle registration number")
+    cost_price = fields.Float(string="Cost Price", required=True, digits=(10,2), help="Purchase price of vehicle")
+    selling_price = fields.Float(string="Selling Price", digits=(10,2))
+    profit = fields.Float(string="Profit", compute='_compute_profit', store=True, readonly=True)
+    state = fields.Selection([('available', 'Available'),
+                              ('sold', 'Sold'),
+                              ('maintenance', 'Maintenance')
+                              ],
+                             string='Status',
+                             default='available',
+                             required=True
+                             )
+    customer_id = fields.Many2one('res.partner', string="Customer")
+    active = fields.Boolean(default=True)
+
+
+    @api.depends('cost_price', 'selling_price')
+    def _compute_profit(self):
+        for record in self:
+            record.profit = record.selling_price - record.cost_price if (
+                record.selling_price) else 0.0
