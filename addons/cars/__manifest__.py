4d593547c979f63b317ec8ff8d5128ed18fdbfd8
f.ahmad <kfraz93@live.com>
2025-07-21 15:47:36 +0200

    Fix(cars): Correcting typo in manifest file

--- addons/cars/__manifest__.py
+++ addons/cars/__manifest__.py
@@ -1,11 +1,12 @@
 {
     'name': "Cars Management",
     'version': "1.0",
-    'depends': ['base'],
+    'depends': ['base',
+                'mail'],
     'author'    : "Fraz Ahmad",
     'category': "Sales",
     'description': "A module to manage car inventory, sales, and profit tracking.",
-    'date': [
+    'data': [
         'security/ir.model.access.csv',
         'views/car_views.xml',
         'views/car_menus.xml',
