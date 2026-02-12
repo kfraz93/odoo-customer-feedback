13eab3afeae76e1b670e38cf8e1203a76e702f91
Fraz Ahmad <kfraz93@live.com>
2026-02-12 20:37:38 +0100

    docs: updated Readme.md

--- addons/cars/models/car.py
+++ addons/cars/models/car.py
@@ -10,9 +10,9 @@ class Car(models.Model):
     _order = "name asc"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string="Car name", required=True, help="Make and model of the car")
+    name = fields.Char(string="Name", required=True, help="Make and model of the car")
     description = fields.Text(string="Description", help="Detailed description of the car")
-    model_year = fields.Integer(string="Model year", required=True, help="Year car was made")
+    model_year = fields.Integer(string="Year", required=True, help="Year car was made")
     vin = fields.Char(string="VIN", copy=False, help="Vehicle registration number")
     cost_price = fields.Float(string="Cost Price", required=True, digits=(10,2), help="Purchase price of vehicle")
     selling_price = fields.Float(string="Selling Price", digits=(10,2))
